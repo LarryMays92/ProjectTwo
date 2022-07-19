e420887cd17b3df77da46d785e0c88a42ba72c92
LarryMays92 <LarryMays92@ymail.com>
2022-07-19 13:49:12 -0400

    edit route almost completed

--- controllers/quote_routes.js
+++ controllers/quote_routes.js
@@ -3,29 +3,60 @@ const express = require('express')
 const router = express.Router()
 // importing Fruit model to access database
 const Quote = require('../models/quotes')
+const Device = require('../models/device')
  
-// GET - Index
-// localhost:3000/devices
-router.get('/', (req, res) => {
-    //mongoose to find all fruits
-    Quote.find()
-    // return fruits as json
-         .then(quote => {
-            console.log(quote)
-             //res.json(quote)
-            res.render('quotes/index')
-         })
-         .catch(err => {
+
+
+// GET route for displaying an update form
+router.get('/:id/edit', (req, res) => {
+    const quoteId = req.params.id
+
+    Quote.findById(quoteId)
+        .then(quote => {
+            res.render('quotes/edit', {quote})
+        })
+        .catch(err => {
             res.json(err)
-         })
+        })
+
 })
 
-router.get('/myquotes', (req, res) => {
-    // find the fruits associated with the logged in user
-    console.log("a",req.session.userID)
-    Quote.find({ owner: req.session.userId })
+//PUT - Edit
+router.put('/:id', (req, res) => {
+    const quoteId = req.params.id
+
+    Quote.findByIdAndUpdate(quoteId, req.body, { new: true })
         .then(quote => {
-            res.render('quotes/show', )
+            res.redirect(`/quotes/${quote._id}`)
+        })
+        .catch(err => {
+            res.json(err)
+        })
+})
+
+
+
+// GET - Index
+// // localhost:3000/quotes
+// router.get('/', (req, res) => {
+//     //mongoose to find all fruits
+//     Quote.find()
+//     // return quotes as json
+//          .then(quote => {
+//             console.log(quote)
+//              //res.json(quote)
+//             res.render('quotes/index')
+//          })
+//          .catch(err => {
+//             res.json(err)
+//          })
+// })
+// get index/////////////////////
+router.get('/myquotes', (req, res) => {
+    // find the quotes associated with the logged in user
+    Quote.find({ owner: req.session.userId })
+        .then(quotes => {
+            res.render('quotes/index', {quotes} )
         })
         .catch(error => {
             console.log(error)
@@ -36,8 +67,16 @@ router.get('/myquotes', (req, res) => {
 
 // GET - New
 router.get('/newquote', (req, res) => {
-    //mongoose to find all fruits
-   res.render('quotes/new')
+    Device.find({})
+        .then(devices => {
+            res.render('quotes/new',{devices})
+        })
+        .catch(err => {
+            console.log(error)
+            res.json({ error })
+        })
+    //mongoose to find all devices
+   
 })
 
 //POST - Create 
@@ -52,8 +91,8 @@ router.post('/', (req, res) => {
     Quote.create(req.body)
         .then(quote => {
             console.log(quote)
-            // res.json(fruit)
-            res.redirect('/quotes')
+            // res.json(quote)
+            res.redirect('/quotes/myquotes')
         })
         .catch(err => {
             res.json(err)
@@ -67,11 +106,22 @@ router.post('/', (req, res) => {
 //i need a get to show the form
 //this get request needs to find all devices and send them to the quote form ,, allows drop down 
 
+//add show route/:id
+router.get('/:id', (req, res) => {
+    // find the specific quote associated with the logged in user
+    Quote.findById(req.params.id)
+        .populate("device")
+        .then(quote => {
+            console.log("this the quote")
+            console.log(quote)
+            res.render('quotes/show', {quote} )
+        })
+        .catch(error => {
+            console.log(error)
+            res.json({ error })
+        })
+})
 //    res.render('quotes/new', { devices })
 //i need a post to process the new quote
 //so itll send to the new page 
-
-
-
-
 module.exports = router
